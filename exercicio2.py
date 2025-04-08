02dffc6d13556eeeb5c2a7dab6b0ff2d36df3150
karolineManoele <karoline.andrade@aluno.ifsp.edu.br>
2025-04-07 22:00:32 -0300

    não estou conseguindooo

---
+++ exercicio2.py
@@ -0,0 +1,19 @@
+class Domino:
+     def __init__(self, valorA, valorB):
+        self.valorA = valorA
+        self.valorB = valorB
+
+     def mostrarPontos():
+        mostrarA = self.valorA
+        mostrarB = self.valorB
+    
+     def valor():
+        soma =  self.valorA + self.valorB
+
+d1=Domino(2,6)
+d2=Domino(4,3)
+d1.mostrarPontos()
+d2.mostrarPontos()
+print("Total de pontos:", d1.value()+
+d2.value())
+print(d1)    
