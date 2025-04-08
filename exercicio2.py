0a7b8b6bc2c525edca5e2e492e6993837f34914d
karolineManoele <karoline.andrade@aluno.ifsp.edu.br>
2025-04-07 22:18:18 -0300

    2 feita

--- exercicio2.py
+++ exercicio2.py
@@ -1,19 +1,12 @@
-class Domino:
-     def __init__(self, valorA, valorB):
-        self.valorA = valorA
-        self.valorB = valorB
-
-     def mostrarPontos():
-        mostrarA = self.valorA
-        mostrarB = self.valorB
-    
-     def valor():
-        soma =  self.valorA + self.valorB
-
-d1=Domino(2,6)
-d2=Domino(4,3)
-d1.mostrarPontos()
-d2.mostrarPontos()
-print("Total de pontos:", d1.value()+
-d2.value())
-print(d1)    
+class Domino():
+    def _init_(self,faceA=0,faceB=0):
+        self.Faces = (faceA,faceB)
+    def getMostrarPontos(self):
+        return print('Lado A: ',self.Faces[0], 'lado B: ',self.Faces[1])
+    def Valor(self):
+        return self.Faces[0] + self.Faces[1]
+domino1 = Domino(2,6)
+domino2 = Domino(4,3)
+domino1.getMostrarPontos()
+domino2.getMostrarPontos()
+print("total de pontos: ", domino1.Valor() + domino2.Valor())
